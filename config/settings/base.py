fd596fd6ac13952784e9707ad7ec14494b662c19
jo <yohanneshailegiorgis0@gmail.com>
2026-06-20 20:33:01 +0300

    build.sh edited

--- config/settings/base.py
+++ config/settings/base.py
@@ -68,12 +68,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "config.wsgi.application"
 
-DATABASES = {
-    "default": dj_database_url.parse(
-        config("DATABASE_URL"),
-        conn_max_age=600,
-    )
-}
+_database_url = config("DATABASE_URL", default="")
+
+if _database_url:
+    DATABASES = {
+        "default": dj_database_url.parse(_database_url, conn_max_age=600)
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
